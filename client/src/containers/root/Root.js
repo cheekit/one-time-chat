23ebd0b82adf241b44e0fd40cbe0c5fdd78950a5
ganezasan <boss@cheekit.com>
2016-10-25 10:37:41 +0900

    feat(root): add init and connect server

--- client/src/containers/root/Root.js
+++ client/src/containers/root/Root.js
@@ -1,9 +1,10 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import SizeMe from 'react-sizeme';
+import io from 'socket.io-client';
 
-import logo from '../../logo.svg';
 import './root.css';
 
+const socket = io('http://localhost:3001');
 
 const propTypes = {
   size: PropTypes.shape({
@@ -12,18 +13,102 @@ const propTypes = {
   }),
 };
 
-function Root() {
-  return (
-    <div className="Root">
-      <div className="Root-header">
-        <img src={logo} className="Root-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="Root-intro">
-        To get started, edit <code>src/Root.js</code> and save to reload.
-      </p>
-    </div>
-  );
+class Root extends Component {
+  state = {
+    users: [],
+    messages: [],
+    name: ''
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+  }
+
+	componentDidMount() {
+		socket.on('init', this._initialize);
+		// socket.on('send:message', this._messageRecieve);
+		// socket.on('user:join', this._userJoined);
+		// socket.on('user:left', this._userLeft);
+		// socket.on('change:name', this._userChangedName);
+	}
+
+  _initialize = this._initialize.bind(this)
+	_initialize(data) {
+		const { users, name } = data;
+    console.log(data);
+		this.setState({ users, name });
+	}
+
+	// _messageRecieve(message) {
+	// 	var {messages} = this.state;
+	// 	messages.push(message);
+	// 	this.setState({messages});
+	// },
+  //
+	// _userJoined(data) {
+	// 	var {users, messages} = this.state;
+	// 	var {name} = data;
+	// 	users.push(name);
+	// 	messages.push({
+	// 		user: 'APPLICATION BOT',
+	// 		text : name +' Joined'
+	// 	});
+	// 	this.setState({users, messages});
+	// },
+  //
+	// _userLeft(data) {
+	// 	var {users, messages} = this.state;
+	// 	var {name} = data;
+	// 	var index = users.indexOf(name);
+	// 	users.splice(index, 1);
+	// 	messages.push({
+	// 		user: 'APPLICATION BOT',
+	// 		text : name +' Left'
+	// 	});
+	// 	this.setState({users, messages});
+	// },
+  //
+	// _userChangedName(data) {
+	// 	var {oldName, newName} = data;
+	// 	var {users, messages} = this.state;
+	// 	var index = users.indexOf(oldName);
+	// 	users.splice(index, 1, newName);
+	// 	messages.push({
+	// 		user: 'APPLICATION BOT',
+	// 		text : 'Change Name : ' + oldName + ' ==> '+ newName
+	// 	});
+	// 	this.setState({users, messages});
+	// },
+  //
+	// handleMessageSubmit(message) {
+	// 	var {messages} = this.state;
+	// 	messages.push(message);
+	// 	this.setState({messages});
+	// 	socket.emit('send:message', message);
+	// },
+  //
+	// handleChangeName(newName) {
+	// 	var oldName = this.state.user;
+	// 	socket.emit('change:name', { name : newName}, (result) => {
+	// 		if(!result) {
+	// 			return alert('There was an error changing your name');
+	// 		}
+	// 		var {users} = this.state;
+	// 		var index = users.indexOf(oldName);
+	// 		users.splice(index, 1, newName);
+	// 		this.setState({users, user: newName});
+	// 	});
+	// },
+
+	render() {
+    // const { users, messages } = this.state;
+    console.log(this.state);
+
+		return (
+			<div>
+			</div>
+		);
+	}
 }
 
 Root.propTypes = propTypes;
