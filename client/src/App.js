f6cc76a6e862a9a34a72b66a6375425261a48095
ganezasan <boss@cheekit.com>
2016-10-30 23:41:09 +0900

    add(App): add Toolbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Match, Miss } from 'react-router';
-import { Root, Sample, ChatSidebar } from './containers';
+import { Root, Sample, ChatSidebar, ChatToolbar } from './containers';
 import { NotFound } from './components';
 import './App.css';
 
@@ -11,12 +11,14 @@ class App extends Component {
         pattern: '/',
         content: () => <Root />,
         sidebar: () => <ChatSidebar />,
+        toolbar: () => <ChatToolbar />,
         exactly: true,
       },
       {
         pattern: '/sample',
         content: () => <Sample />,
         sidebar: () => null,
+        toolbar: () => null,
         exactly: false,
       },
     ];
@@ -25,6 +27,16 @@ class App extends Component {
       <BrowserRouter>
         <div className="application_wrapper">
           <noscript></noscript>
+          <div className="application_header">
+            {routes.map((route, index) => (
+              <Match
+                key={index}
+                pattern={route.pattern}
+                component={route.toolbar}
+                exactly={route.exactly}
+              />
+            ))}
+          </div>
           <div className="application_content">
             <section className="search">
               <div className="search_content">
