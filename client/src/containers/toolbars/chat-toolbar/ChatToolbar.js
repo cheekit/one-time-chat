70064af4525d94a1a9798f284fad1d81fcba6f6b
ganezasan <boss@cheekit.com>
2016-10-30 23:39:25 +0900

    feat(ChatToolbar): add ChatToolbar

---
+++ client/src/containers/toolbars/chat-toolbar/ChatToolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Toolbar } from '../../../components';
+import {
+  HomeControl
+} from '../../controls';
+
+function ChatToolbar() {
+  return (
+    <Toolbar>
+      <HomeControl />
+    </Toolbar>
+  );
+}
+
+export default ChatToolbar;
---
+++ client/src/containers/toolbars/index.js
@@ -0,0 +1,1 @@
+export { default as ChatToolbar } from './chat-toolbar';
