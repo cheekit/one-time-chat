9af3fb7b359b2a4784e8adb6ab14e3914f790b73
ganezasan <boss@cheekit.com>
2016-10-30 11:25:25 +0900

    fix(Root): change tab to space

--- client/src/containers/root/Root.js
+++ client/src/containers/root/Root.js
@@ -27,14 +27,14 @@ class Root extends Component {
     message: '',
   }
 
-	componentDidMount() {
+  componentDidMount() {
     window.addEventListener('resize', this.handleResize);
-		socket.on('init', this._initialize);
-		socket.on('send:message', this._messageRecieve);
-		socket.on('user:join', this._userJoined);
-		// socket.on('user:left', this._userLeft);
-		// socket.on('change:name', this._userChangedName);
-	}
+    socket.on('init', this._initialize);
+    socket.on('send:message', this._messageRecieve);
+    socket.on('user:join', this._userJoined);
+    // socket.on('user:left', this._userLeft);
+    // socket.on('change:name', this._userChangedName);
+  }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleResize);
@@ -58,64 +58,64 @@ class Root extends Component {
   }
 
   _initialize = this._initialize.bind(this)
-	_initialize(data) {
-		const { users, name } = data;
-		this.setState({ users, name });
-	}
+  _initialize(data) {
+    const { users, name } = data;
+    this.setState({ users, name });
+  }
 
   _messageRecieve = this._messageRecieve.bind(this)
-	_messageRecieve(message) {
-		const { messages } = this.state;
-		this.setState(appendMessage(message.user, message.message, messages));
-	}
+  _messageRecieve(message) {
+    const { messages } = this.state;
+    this.setState(appendMessage(message.user, message.message, messages));
+  }
 
   _userJoined = this._userJoined.bind(this)
-	_userJoined(user) {
-		const {users, messages} = this.state;
-		users.push(user);
-	  messages.push({
-			user: 'APPLICATION BOT',
-			message : user.name +' Joined'
-		});
-		this.setState({users, messages});
-	}
+  _userJoined(user) {
+    const {users, messages} = this.state;
+    users.push(user);
+    messages.push({
+      user: 'APPLICATION BOT',
+      message : user.name +' Joined'
+    });
+    this.setState({users, messages});
+  }
 
-	// _userLeft(data) {
-	// 	var {users, messages} = this.state;
-	// 	var {name} = data;
-	// 	var index = users.indexOf(name);
-	// 	users.splice(index, 1);
-	// 	messages.push({
-	// 		user: 'APPLICATION BOT',
-	// 		message : name +' Left'
-	// 	});
-	// 	this.setState({users, messages});
-	// },
+  // _userLeft(data) {
+  //   var {users, messages} = this.state;
+  //   var {name} = data;
+  //   var index = users.indexOf(name);
+  //   users.splice(index, 1);
+  //   messages.push({
+  //     user: 'APPLICATION BOT',
+  //     message : name +' Left'
+  //   });
+  //   this.setState({users, messages});
+  // },
   //
-	// _userChangedName(data) {
-	// 	var {oldName, newName} = data;
-	// 	var {users, messages} = this.state;
-	// 	var index = users.indexOf(oldName);
-	// 	users.splice(index, 1, newName);
-	// 	messages.push({
-	// 		user: 'APPLICATION BOT',
-	// 		message : 'Change Name : ' + oldName + ' ==> '+ newName
-	// 	});
-	// 	this.setState({users, messages});
-	// },
+  // _userChangedName(data) {
+  //   var {oldName, newName} = data;
+  //   var {users, messages} = this.state;
+  //   var index = users.indexOf(oldName);
+  //   users.splice(index, 1, newName);
+  //   messages.push({
+  //     user: 'APPLICATION BOT',
+  //     message : 'Change Name : ' + oldName + ' ==> '+ newName
+  //   });
+  //   this.setState({users, messages});
+  // },
   //
-	// handleChangeName(newName) {
-	// 	var oldName = this.state.user;
-	// 	socket.emit('change:name', { name : newName}, (result) => {
-	// 		if(!result) {
-	// 			return alert('There was an error changing your name');
-	// 		}
-	// 		var {users} = this.state;
-	// 		var index = users.indexOf(oldName);
-	// 		users.splice(index, 1, newName);
-	// 		this.setState({users, user: newName});
-	// 	});
-	// },
+  // handleChangeName(newName) {
+  //   var oldName = this.state.user;
+  //   socket.emit('change:name', { name : newName}, (result) => {
+  //     if(!result) {
+  //       return alert('There was an error changing your name');
+  //     }
+  //     var {users} = this.state;
+  //     var index = users.indexOf(oldName);
+  //     users.splice(index, 1, newName);
+  //     this.setState({users, user: newName});
+  //   });
+  // },
   handleUpdateMessage = this.handleUpdateMessage.bind(this);
   handleUpdateMessage(e) {
     this.setState({message: e.target.value});
@@ -127,7 +127,7 @@ class Root extends Component {
     const { messages, message, name } = this.state;
 
     const newMessages = appendMessage(name, message, messages);
-		socket.emit('send:message', message);
+    socket.emit('send:message', message);
 
     this.setState({
       message: '',
@@ -148,24 +148,24 @@ class Root extends Component {
     );
   }
 
-	render() {
+  render() {
     const { messages, message, height } = this.state;
     const formHeight = 40;
     const messagesStyle = {
       height: height - formHeight,
     };
 
-		return (
-			<div style={{height: height}}>
+    return (
+      <div style={{height: height}}>
         <ul className={"messages"} style={messagesStyle}>
           {messages.map((m, i) => this.renderMessage(m.user, m.message, i))}
         </ul>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleUpdateMessage} value={message}/><button>Send</button>
         </form>
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
 
 export default Root;
