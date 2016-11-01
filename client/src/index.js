5524cbc6e65d95f0b043acf949390cb226f531da
ganezasan <boss@cheekit.com>
2016-11-01 09:19:56 +0900

    feat(App): add material ui

--- client/src/index.js
+++ client/src/index.js
@@ -8,15 +8,37 @@ import configureStore from './redux/configureStore';
 //auth
 import { initAuth } from './redux/auth';
 
+//material ui
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import App from './App';
 import './index.css';
 
+// Some components use react-tap-event-plugin to listen for touch events because
+// onClick is not fast enough This dependency is temporary and will eventually go away.
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 const store = configureStore();
+
+const muiTheme = getMuiTheme({
+  fontFamily: 'Roboto, sans-serif',
+  fontWeight: 300,
+  palette: {
+    primary1Color: '#464d5e',
+    primary2Color: '#5c657b',
+    primary3Color: '#5c657b',
+  }
+});
 
 function render(Routes) {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+      </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
   );
