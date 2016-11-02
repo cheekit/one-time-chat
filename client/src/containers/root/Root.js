0f9acc41f7722f564214a684193a8d86fadf99ea
ganezasan <boss@cheekit.com>
2016-11-02 22:21:13 +0900

    fix(Root): fix root view

--- client/src/containers/root/Root.js
+++ client/src/containers/root/Root.js
@@ -1,8 +1,4 @@
 import React, { PropTypes, Component } from 'react';
-import { connect } from 'react-redux';
-import { initSocket, sendMessage } from '../../redux/chat';
-import { ChatSidebar } from '../sidebars';
-
 import './root.css';
 
 const propTypes = {
@@ -11,108 +7,29 @@ const propTypes = {
 };
 
 class Root extends Component {
-  state = {
-    height: document.documentElement.clientHeight,
-    sockets: {
-      messages: [],
-    },
-    message: '',
-  }
-
-  componentDidMount() {
-    const { dispatch } = this.props;
-
-    dispatch(initSocket());
-
-    window.addEventListener('resize', this.handleResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { sockets } = nextProps;
-
-    if (sockets) this.setState({ sockets });
-  }
-
-  handleResize = this.handleResize.bind(this);
-  handleResize() {
-    this.setState({
-      height: document.querySelector('.application_content').clientHeight,
-    });
-  }
-
-  handleUpdateMessage = this.handleUpdateMessage.bind(this);
-  handleUpdateMessage(e) {
-    this.setState({message: e.target.value});
-  }
-
-  handleSubmit = this.handleSubmit.bind(this);
-  handleSubmit(e) {
-    e.preventDefault();
-    const { dispatch } = this.props;
-    const { message, sockets } = this.state;
-    const { name } = sockets;
-
-    dispatch(sendMessage(name, message))
-
-    this.setState({
-      message: '',
-    });
-  }
-
-  renderMessage(user, message, i) {
-    const userNameStyle = {
-      fontSize: '9px',
-      color: '#aaa',
-    };
-    return (
-      <li key={i}>
-        <span style={userNameStyle}>{user}</span><br />
-        {message}
-      </li>
-    );
-  }
-
   render() {
-    const { sockets, message, height } = this.state;
-    const { messages } = sockets;
-    const formHeight = 40+64;
-    const messagesStyle = {
-      height: height - formHeight,
-    };
-
     return (
-      <section className="search">
-        <div className="search_content">
-          <div className="search_inner_content">
-            <div style={{height: height}}>
-              <ul className={"messages"} style={messagesStyle}>
-                {messages.map((m, i) => this.renderMessage(m.user, m.message, i))}
-              </ul>
-              <form onSubmit={this.handleSubmit}>
-                <input onChange={this.handleUpdateMessage} value={message}/><button>Send</button>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div className="search_sidebar">
-          <ChatSidebar />
-        </div>
-      </section>
+      <div>
+        <h1>Root</h1>
+        <p> After it'll be show chat rooms</p>
 
+        <h2>Todo</h2>
+        <ul>
+          <li>Add Room</li>
+          <li>Show Rooms</li>
+          <li>Join Rooms</li>
+          <li>Send Message (text)</li>
+          <li>Send Message (image)</li>
+          <li>Send Message (link)</li>
+          <li>Edit Message</li>
+          <li>Delete Message</li>
+          <li>Close room after 15 minutes</li>
+        </ul>
+      </div>
     );
   }
-}
-
-function mapStateToProps(state) {
-  const { sockets } = state.socketsReducer;
-
-  return { sockets };
 }
 
 Root.propTypes = propTypes;
 
-export default connect(mapStateToProps)(Root);
+export default Root;
