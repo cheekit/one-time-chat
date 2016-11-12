70c03393a36c6ccd955a1fda9810476c467cda1b
ganezasan <boss@cheekit.com>
2016-11-12 10:53:49 +0900

    feat(redux): add channel class

--- client/src/redux/channels/channel.js
+++ client/src/redux/channels/channel.js
@@ -1,9 +1,38 @@
 import { Record } from 'immutable';
 
-export const Channel = new Record({
+export class Channel extends Record({
   key: null,
+  userUid: null,
   private: false,
   name: null,
   purpose: null,
-  invites: []
-});
+  invites: [],
+  createdAt: null,
+  updatedAt: null
+}) {
+  constructor(props = {}) {
+    const { key, userUid, name, privateFlg, purpose, invites, createdAt, updatedAt } = props;
+
+    super({
+      key: key || null,
+      userUid: userUid || null,
+      name: name || '',
+      private: privateFlg || false,
+      purpose: purpose || '',
+      invites: invites || [],
+      createdAt: createdAt || new Date(),
+      updatedAt: updatedAt || new Date(),
+    });
+  }
+}
+
+// export const Channel = new Record({
+//   key: null,
+//   userUid: null,
+//   private: false,
+//   name: null,
+//   purpose: null,
+//   invites: [],
+//   createdAt: null,
+//   updatedAt: null
+// });
